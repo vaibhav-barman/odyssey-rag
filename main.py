1b46f355f5c07346899f0216a53ddcda82b448c9
Vaibhav Barman <vaibhavbarman2609@gmail.com>
2026-09-23 20:49:41 +0530

    Initial Gemini RAG project setup

---
+++ main.py
@@ -0,0 +1,16 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
+response = client.models.generate_content(
+    model="gemini-3.5-flash-lite",
+    contents="Explain RAG in one sentence"
+)
+
+print(response.text)
