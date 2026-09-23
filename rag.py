3335579207c33419791a5da04ef58e8ec48d8889
Vaibhav Barman <vaibhavbarman2609@gmail.com>
2026-09-24 00:25:19 +0530

    Add document chunking

--- rag.py
+++ rag.py
@@ -4,7 +4,21 @@ def load_document(file_path):
 
     return text
 
+def chunk_text(text, chunk_size=1000):
+    chunks = []
+
+    for i in range(0, len(text), chunk_size):
+        chunk = text[i: i + chunk_size]
+        chunks.append(chunk)
+
+    return chunks
+
 text = load_document("data/the-odyssey.txt")
 
 print('Characters:', len(text))
-print(text[:2000])
+
+chunks = chunk_text(text)
+
+print("Number of chunks:", len(chunks))
+print("\nFirst Chunk:\n")
+print(chunks[0])
